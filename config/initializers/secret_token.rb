63da8bc5c94efe25b4a9d3ed558dea1f00d67e92
Your Name <you@example.com>
2013-04-17 20:17:23 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Choibg::Application.config.secret_token = '2744235e3ef62f23c38c65a63aa24175809bbba99262f2d538ea4566df016e3f8b4a5d85bdc6b960df7a8779a7a938ab103299c71b245ba5cf8971b81e44de17'
